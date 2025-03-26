e0394c8628d944f7a84ac21794824557cc23c1f9
NaufalR12 <naufal.rafid.mf@gmail.com>
2025-03-26 14:14:42 +0700

    tugas 6 tcc

--- frontend/script/script.js
+++ frontend/script/script.js
@@ -1,5 +1,5 @@
 const apiUrl =
-  "https://backend-tugas5-505940949397.us-central1.run.app:5000/api/notes";
+  "https://backend-tugas5-505940949397.us-central1.run.app/api/notes";
 
 document.getElementById("addNoteBtn").addEventListener("click", () => {
   document.getElementById("formTitle").innerText = "New Note";
