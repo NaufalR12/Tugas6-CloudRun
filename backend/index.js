083880746b846da723dd28ee47ae6d5610b7b9d4
NaufalR12 <naufal.rafid.mf@gmail.com>
2025-05-21 12:02:23 +0700

    rev 1202

--- backend/index.js
+++ backend/index.js
@@ -7,28 +7,13 @@ const cookieParser = require("cookie-parser");
 const app = express();
 
 // CORS configuration
-const corsOptions = {
-  origin: [
-    'https://frontend-nopal-dot-b-08-450916.uc.r.appspot.com',
-    'http://localhost:3000',
-    'http://localhost:5000'
-  ],
+app.use(cors({
+  origin: 'https://frontend-nopal-dot-b-08-450916.uc.r.appspot.com',
+  credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'Origin', 'X-Requested-With'],
-  exposedHeaders: ['Content-Range', 'X-Content-Range', 'Set-Cookie'],
-  credentials: true,
-  preflightContinue: false,
-  optionsSuccessStatus: 204
-};
-
-// Terapkan CORS sebelum middleware lainnya
-app.use(cors(corsOptions));
-
-// Tambahkan header CORS untuk semua response
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Credentials', 'true');
-  next();
-});
+  exposedHeaders: ['Set-Cookie']
+}));
 
 // Terapkan middleware
 app.use(cookieParser());
